02d8ad1b771735a9e1469d0b9b8ac20224e2c916
Luciano Milen <luciano.milen@britecore.com>
2019-09-27 10:20:21 -0300

    Add braces to switch

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,16 +1,18 @@
 const reducer = (state, action) => {
   switch (action.type) {
-    case 'login':
+    case 'login': {
       return {
         ...state,
         loggedUser: action.newUser
       }
-    case 'setList':
+    }
+    case 'setList': {
       const { entityState, listName } = action.payload.path
       state[entityState][listName] = action.payload.list
       return {
         ...state
       }
+    }
     default:
       return state
   }
